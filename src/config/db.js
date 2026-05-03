a42e0d51b8f887c2f024e4df5a5bdd14a1e28b0b
khang tran <qkhang1545678@gmail.com>
2026-05-03 09:40:01 +0700

    1

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_CONNECTIONSTRING);
-    console.log("Connected to the database");
+    console.log("Connected to the database 22");
   } catch (error) {
     console.error("lỗi kết nối csdl", error);
     process.exit(1); // dừng chương trình nếu có lỗi
